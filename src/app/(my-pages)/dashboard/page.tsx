f76e89ee7ab1a820a8f44cb36f34fda88db4f98f
Ganda Tadio Surya <gandatadyosurya@gmail.com>
2024-10-03 16:50:25 +0700

    init

---
+++ src/app/(my-pages)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Home() {
+
+  return (
+    <div className="p-5">
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
---
+++ src/app/(my-pages)/register/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Home() {
+
+  return (
+    <div className="p-5">
+      <h1>Register</h1>
+    </div>
+  );
+}
