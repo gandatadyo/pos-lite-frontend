308a96f9c8945d2da46d4ed4bd6d602cb9b82a34
gandatadyo <gandatadyosurya@gmail.com>
2024-10-22 12:57:55 +0700

    update

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -11,7 +11,7 @@ interface ButtonProps {
     return (
       <button
         onClick={onClick}
-        className={`px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 ${className}`}
+        className={`px-4 py-2 rounded  text-white  ${className}`}
         disabled={disabled}
       >
         {children}
